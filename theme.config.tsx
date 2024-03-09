52c05339228c861810eaded0251714ae18e6d992
Axole Maranjana <axolemaranjana4@gmail.com>
2024-03-09 17:03:08 +0200

    Update contact link and project link in _meta.json and theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>My Project</span>,
+	project: {
+		link: "https://github.com/attlr-ui/docs",
+	},
+	// chat: {
+	// 	link: "https://discord.com",
+	// },
+	docsRepositoryBase: "https://github.com/attlr-ui/docs",
+	footer: {
+		component: <div>Axole</div>,
+	},
+	useNextSeoProps() {
+		return {
+			titleTemplate: "%s – Attlr UI Docs",
+			description: "Documentation for Attlr UI",
+			openGraph: {
+				siteName: "Attlr UI Docs",
+				title: "Attlr UI Docs",
+				description: "Documentation for Attlr UI",
+				locale: "en_US",
+			},
+		};
+	},
+	primaryHue: {
+		dark: 500,
+		light: 530,
+	},
+};
 
-export default config
+export default config;
