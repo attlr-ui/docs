5d6fc486aadadebc1d9fa5f98cb6e5d3989636a1
Axole Maranjana <axolemaranjana4@gmail.com>
2024-03-09 21:39:33 +0200

    Update package.json description

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 })
 
 module.exports = withNextra()
