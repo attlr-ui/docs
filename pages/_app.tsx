f0648441abc4a941440f16ec73ceab4dd88040d3
Axole Maranjana <axolemaranjana4@gmail.com>
2024-03-10 13:25:22 +0200

    Add styles import and default export to _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import '../styles.css'
+import '../style.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
